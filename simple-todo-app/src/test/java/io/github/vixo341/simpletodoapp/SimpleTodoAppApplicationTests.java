b72c6da7034646e645012dd8ebf5ea28e34f4581
vixo341 <malpa341@gmail.com>
2020-08-07 22:36:25 +0200

    initial project

---
+++ simple-todo-app/src/test/java/io/github/vixo341/simpletodoapp/SimpleTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vixo341.simpletodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
